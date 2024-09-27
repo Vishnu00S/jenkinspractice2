d7996162d542f0748f1f3eadcc13eb28d29c6514
Vishnu00S <94123890+Vishnu00S@users.noreply.github.com>
2024-09-27 22:06:41 +0530

    2nd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi ... How are you");
-        System.out.println("Hi ... I'm Fine");
-        System.out.println("Thank you");
-     
-        System.out.println("Hi ... Luffy How are you");
-        System.out.println("Hi ... Zoro I'm Fine");
- System.out.println("Hi ... Zoro I'm Fine"
-        
         
     }
 }
